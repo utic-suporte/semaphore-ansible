5ce2e5174ecd7c8c78ec454bfa227b7a2742fea1
utic-suporte <109242201+utic-suporte@users.noreply.github.com>
2024-07-29 08:42:44 -0300

    Create alterar_senha_rustdesk.ps1

---
+++ alterar_senha_rustdesk.ps1
@@ -0,0 +1,27 @@
+# Caminho para o arquivo de configuração do RustDesk
+$configFilePath = "$env:ProgramFiles\RustDesk\config\RustDesk.toml"
+
+# Nova senha que você quer definir
+$newPassword = "novaSenhaAqui"
+
+# Função para parar o serviço do RustDesk
+function Stop-RustDeskService {
+    Stop-Service -Name "RustDesk" -Force
+}
+
+# Função para iniciar o serviço do RustDesk
+function Start-RustDeskService {
+    Start-Service -Name "RustDesk"
+}
+
+# Função para alterar a senha no arquivo de configuração
+function Set-RustDeskPassword {
+    $configContent = Get-Content -Path $configFilePath -Raw
+    $newConfigContent = $configContent -replace 'password = ".*"', "password = `"$newPassword`""
+    Set-Content -Path $configFilePath -Value $newConfigContent -Force
+}
+
+# Executar as funções
+Stop-RustDeskService
+Set-RustDeskPassword
+Start-RustDeskService
