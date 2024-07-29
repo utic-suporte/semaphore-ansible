82a94afa3e6ad7eca9527c44b09b6a0dca13129b
utic-suporte <109242201+utic-suporte@users.noreply.github.com>
2024-07-29 11:37:22 -0300

    Update alterar_senha_rustdesk.ps1

--- alterar_senha_rustdesk.ps1
+++ alterar_senha_rustdesk.ps1
@@ -6,14 +6,23 @@ $config_path = "C:\Users\$current_user\AppData\Roaming\RustDesk\config"
 
 # Certifique-se de que o diretório de configuração existe
 if (Test-Path -Path $config_path) {
-    # Caminho completo do arquivo de configuração (adaptar conforme o nome do arquivo real)
+    # Caminho completo do arquivo de configuração
     $config_file = "$config_path\Rustdesk.toml"
 
     # Certifique-se de que o arquivo de configuração existe
     if (Test-Path -Path $config_file) {
-        # Alterar a senha no arquivo de configuração
-        # Exemplo: substituindo a linha que contém a senha (adaptar conforme o formato real do arquivo de configuração)
-        (Get-Content $config_file) -replace '00vU35ke9AvERTkLsbZfJDcu/rKPqqHTg6', 'nova_senha' | Set-Content $config_file
+        # Nova senha para substituir
+        $nova_senha = 'NovaSenhaAqui'
+
+        # Ler o conteúdo do arquivo de configuração
+        $config_content = Get-Content $config_file
+
+        # Substituir a linha que contém a senha
+        $config_content = $config_content -replace "password = '.*'", "password = '$nova_senha'"
+
+        # Gravar o conteúdo atualizado de volta no arquivo
+        $config_content | Set-Content $config_file
+
         Write-Output "Senha alterada com sucesso."
     } else {
         Write-Output "Arquivo de configuração não encontrado: $config_file"
